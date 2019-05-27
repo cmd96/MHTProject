4fbe48dc0183770defac3bedc256f7fd403d4ec6
Shira David <shiranaamaedrey@gmail.com>
2019-05-27 13:44:59 +0300

    Update Project with all his flow

--- src/Project/db/managers/ProjectManager.java
+++ src/Project/db/managers/ProjectManager.java
@@ -4,6 +4,9 @@ import Creat_Projects.Create_Project;
 import Project.exception.ProjectExeption;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
 
 public interface  ProjectManager {
     public interface MileStonManager {
@@ -12,6 +15,34 @@ public interface  ProjectManager {
         }
 
         static void Update_Project(Connection con, Create_Project project) throws ProjectExeption {
+            //        Create_Project pro = project.get_Project(con, project.getProjectID());
+            System.out.println(project.getNumsOfMiles());
+            System.out.println(project.getCompany_Repository());
+            System.out.println(project.getDateDone());
+            System.out.println(project.getTDescription());
+            System.out.println(project.getProjectManagmentID());
+            System.out.println("in update " +project.getProjectID());
+            try{
+                String sql;
+                sql = "UPDATE Projects SET ";
+//            sql += "ID = '" + MileSton.create_Mile_Id() + "', ";
+                sql += "ProjectManagmentID = '" + project.getProjectManagmentID() + "', ";
+                sql += "ResponsiboleWriterId = '" + project.getResponsiboleWriterId() + "', ";
+                sql += "TDescription = '" + project.getTDescription() + "', ";
+                sql += "NumsOfMiles = '" + project.getNumsOfMiles() + "', ";
+//            sql += "DateStart = '" + project.getDateStart() + "' ";
+//            sql += "DateDone = '" + project.getDateDone();
+                sql += "Company_Repository = '" + project.getCompany_Repository() + "' ";
+                sql += "WHERE ID = " + project.getProjectID();
+                System.out.println("id "+ project.getProjectID());
+                Statement stmt = con.createStatement();
+                stmt.executeUpdate(sql);
+            }
+            catch (SQLException e) {
+                throw new ProjectExeption("failed to update Project", e);
+            }
+
+
         }
 
         static void Copy_Project(Connection con, int project_num) throws ProjectExeption {
@@ -19,7 +50,7 @@ public interface  ProjectManager {
             Create_Project new_pro;
             new_pro = new Create_Project((int)projet_to_copy.getProjectID(),
                     (int)projet_to_copy.getProjectManagmentID(),projet_to_copy.getResponsiboleWriterId(),
-                    (String) projet_to_copy.getTDescription(),projet_to_copy.getNumsOfMiles(),(Date)projet_to_copy.getDateStart(),(Date)projet_to_copy.getDateDone(),
+                    (String) projet_to_copy.getTDescription(),projet_to_copy.getNumsOfMiles(),(Date) projet_to_copy.getDateStart(),(Date)projet_to_copy.getDateDone(),
                     (String)projet_to_copy.getCompany_Repository());
             insert(con,new_pro);
 //        try {
