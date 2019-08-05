2363ab2bd0c10a27f64e59548e62b4ee17fa0e83
shiraed <33096142+shiraed@users.noreply.github.com>
2019-08-06 00:53:08 +0300

    Update User.java

--- src/com/test/User.java
+++ src/com/test/User.java
@@ -44,12 +44,101 @@ public class User {
         this.userProjectList = projectList;
     }
     //fill project
-    //{
-    //  for list projectList from SQLserver
+       public List get_Project(Connection con, int user_id) throws ProjectExeption {
+//        qwery that return the list of project
+        List<Create_Project> create_projects= new ArrayList<>();
+        String sql= "Select * from Projects WHERE ProjectManager = "+ user_id;
+        try {
+
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next())
+        {
+            create_projects.add(new Create_Project(
+                    rs.getInt("ID"),
+                    rs.getString("name"),
+                    rs.getInt("ProjectManager"),
+                    rs.getString("Costumer"),
+                    get_Products(con, rs.getInt("ID"))));
+        }
+            } catch (SQLException e) {
+            throw new ProjectExeption("Get the MileSton  faild!!", e);
+        }
+
+
+        return (List) create_projects;
+    }
+
     //}
     //fill product data
+    public java.awt.List get_Products(Connection con, int project_num) throws ProjectExeption {
+        List<ProjectProduct> productList = new ArrayList<>();
+        List<Product> p = new ArrayList<>();
+        String sql = "SELECT * FROM Product WHERE ID="+ project_num;
+        try {
 
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next())
+            {
+//                productList.add(new ProjectProduct(rs.getInt("projectID"),
+//                                rs.getString("productName"),
+//                                rs.getInt("productID"),
+//                        (java.awt.List)get_MileStons(con,rs.getInt("projectID"),rs.getInt("ID")),
+//                                rs.getInt("template_flow")));
+
+//using in my class  ->     Product BUT---
+// I think that need to use with ProjectProduct
+
+                p.add(new Product(rs.getInt("ID"),
+                        rs.getString("name"),
+                        rs.getInt("projectID"),
+                        get_MileStons(con, rs.getInt("projectID"), rs.getInt("ID"))));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return (java.awt.List) productList;
+
+
+    }
     //fill milestone
+    public static java.awt.List get_MileStons(Connection con, int project_num,  int product_num) throws ProjectExeption {
+        java.lang.String sql = "Select * from MileSton WHERE projectID = " + project_num +
+                "AND poductID " + "= " + product_num;
+
+        List<ProductMilestone> mileStonList = new ArrayList<>();
+        List<MileSton> mile = new ArrayList<>();
+        try {
+
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+//                mileStonList.add(new ProductMilestone
+//                        (rs.getInt("productID"),
+//                                rs.getString("milestoneName"),
+//                                rs.getInt("ResponsibleWriterId"),
+//                                rs.getInt("ResponsibleWriter"),
+//                                rs.getDate("startDate"),
+//                                rs.getDate("endDate"),
+//                                rs.getString("description"),
+//                                rs.getInt("status")));
+//            }
+//                {
+                mile.add(new MileSton(rs.getInt("ID"),
+                        rs.getString("name"),
+                        rs.getInt("poductID"),
+                        rs.getInt("projectID"),
+                        rs.getInt("ResponsibleWriter"),
+                        rs.getDate("DateStart"),
+                        rs.getDate("DateToEnd"),
+                        rs.getString("Description")));
+            }
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
+        return (java.awt.List) mile;
+}
 
     public static String getUserName() throws SQLException, ClassNotFoundException {
         String fullName="";
