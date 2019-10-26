66174627dc6c85c9493a38ff3bde7b56af6fd503
Shira David <shiranaamaedrey@gmail.com>
2019-10-26 23:36:17 +0300

    Set productNmae, projectID, ResponsibleID, FlowType in Data Base

--- src/com/test/ProjectProduct.java
+++ src/com/test/ProjectProduct.java
@@ -12,7 +12,7 @@ public class ProjectProduct {
     private int productID;
     private int ResponsibleWriterId;
     private List<ProductMilestone> productMilestone;
-    private int [] template_flow;
+    private int[] template_flow;
 
 
     public ProjectProduct(int projectID, String productName, int productID, int responsibleWriterId, List<ProductMilestone> productMilestone, int[] template_flow) {
@@ -23,6 +23,7 @@ public class ProjectProduct {
         this.productMilestone = productMilestone;
         this.template_flow = template_flow;
     }
+
     public int getResponsibleWriterId() {
         return ResponsibleWriterId;
     }
@@ -30,6 +31,7 @@ public class ProjectProduct {
     public void setResponsibleWriterId(int responsibleWriterId) {
         ResponsibleWriterId = responsibleWriterId;
     }
+
     public int getProjectID() {
         return projectID;
     }
@@ -61,6 +63,7 @@ public class ProjectProduct {
     public void setProductMilestone(List<ProductMilestone> productMilestone) {
         this.productMilestone = productMilestone;
     }
+
     public int[] getTemplate_flow() {
         return template_flow;
     }
@@ -69,78 +72,113 @@ public class ProjectProduct {
         this.template_flow = template_flow;
     }
 
-    public String getHtmlCode()
-    {
+    public String getHtmlCode() {
         String htmlCode = CONST_HTML_CODE.HTML_PRODUCT_UP_WRAPPER_START;
-        htmlCode+= CONST_HTML_CODE.HTML_PRODUCT_WRAPPER_START + CONST_HTML_CODE.HTML_PRODUCT_CODE ;
-        for (ProductMilestone milesotne: productMilestone) {
+        htmlCode += CONST_HTML_CODE.HTML_PRODUCT_WRAPPER_START + CONST_HTML_CODE.HTML_PRODUCT_CODE;
+        for (ProductMilestone milesotne : productMilestone) {
 
-            htmlCode+=milesotne.getHtmlCode();
+            htmlCode += milesotne.getHtmlCode();
         }
-        htmlCode+=CONST_HTML_CODE.HTML_PRODUCT_WRAPPER_END;
-        htmlCode+=CONST_HTML_CODE.HTML_PRODUCT_BTN;
-        htmlCode+=CONST_HTML_CODE.HTML_PRODUCT_UP_WRAPPER_END;
+        htmlCode += CONST_HTML_CODE.HTML_PRODUCT_WRAPPER_END;
+        htmlCode += CONST_HTML_CODE.HTML_PRODUCT_BTN;
+        htmlCode += CONST_HTML_CODE.HTML_PRODUCT_UP_WRAPPER_END;
         htmlCode = htmlCode.replaceAll(CONST_HTML_CODE.PLACEHOLDER_PRODUCT_NAME, getProductName());
         return htmlCode;
     }
 
     //TODO: insert new product to current project
     public static void InsertNewProduct(ProjectProduct insert) throws ProjectExeption, SQLException, ClassNotFoundException {
-        Connection con=SQLConnection.getCon();
+        Connection con = SQLConnection.getCon();
         String Name = insert.getProductName();
         int ProjID = insert.getProjectID();
         int rw = insert.getResponsibleWriterId();
         List<ProductMilestone> productMilestone = insert.getProductMilestone();
 
-        int [] template_flow = insert.getTemplate_flow();
+        int[] template_flow = insert.getTemplate_flow();
         insert_product_db(con, Name, 14, rw);
-        if (productMilestone != null)
-        {
+        if (productMilestone != null) {
             int product_id = ( int ) get_product_id(con, Name, ProjID);
             ProductMilestone.insert_MileSton_list(con, productMilestone, product_id);
 //            System.out.println("the new product id is :" + product_id);
         }
     }
-    public static void insert_product_db(Connection con, String Name, int ProjID, int rw){
-        String sql = "INSERT INTO ProjectProduct (ProductName ,ProjectID, ResponsibleWriterID) VALUES ( '"+Name +"','"+ProjID+"','"+rw+"')";
+
+    public static void insert_product_db(Connection con, String Name, int ProjID, int rw) {
+        String sql = "INSERT INTO ProjectProduct (ProductName ,ProjectID, ResponsibleWriterID) VALUES ( '" + Name + "','" + ProjID + "','" + rw + "')";
         try {
             Statement stmt = con.createStatement();
             stmt.executeUpdate(sql);
 
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
     }
 
-    public static int get_product_id(Connection con, String Name, int ProjID){
-        String sql = "Select ProductID from ProjectProduct where ProductName like '"+Name+"' and ProjectID like '"+ProjID+"'";
+    public static int get_product_id(Connection con, String Name, int ProjID) {
+        String sql = "Select ProductID from ProjectProduct where ProductName like '" + Name + "' and ProjectID like '" + ProjID + "'";
         int id = -1;
         try {
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery(sql);
-            while (rs.next()){
+            while (rs.next()) {
                 id = rs.getInt("ProductID");
             }
 
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return id;
 
 
     }
-    public static void setProductNameDB(Connection con, String NewName){
-        String sql = "update ProjectProduct set ProductName = '"+NewName+"'";
+
+    public static void setProductNameDB(Connection con, String NewName) {
+        String sql = "update ProjectProduct set ProductName = '" + NewName + "'";
         try {
-          Statement stmt = con.createStatement();
-          int count = stmt.executeUpdate(sql);
+            Statement stmt = con.createStatement();
+            int count = stmt.executeUpdate(sql);
             System.out.println("Number of rows updated by executing query1 =  " + count);
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
+    }
+
+    public static void setProjectIDDB(Connection con, int NewProjID) {
+        String sql = "update ProjectProduct set ProjectID = '" + NewProjID + "'";
+        try {
+            Statement stmt = con.createStatement();
+            int count = stmt.executeUpdate(sql);
+            System.out.println("Number of rows updated by executing query1 =  " + count);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setResponsibleWriterIDDB(Connection con, int NewResponsibleID) {
+        String sql = "update ProjectProduct set ResponsibleWriterID = '" + NewResponsibleID + "'";
+        try {
+            Statement stmt = con.createStatement();
+            int count = stmt.executeUpdate(sql);
+            System.out.println("Number of rows updated by executing query1 =  " + count);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void setFlowTypeDB(Connection con, int NewFlowType) {
+        String sql = "update ProjectProduct set FlowType = '" + NewFlowType + "'";
+        try {
+            Statement stmt = con.createStatement();
+            int count = stmt.executeUpdate(sql);
+            System.out.println("Number of rows updated by executing query1 =  " + count);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
